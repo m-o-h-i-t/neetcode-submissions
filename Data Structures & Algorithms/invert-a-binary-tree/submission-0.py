71fbccc3985e198cdcccdb546f4da6302ff4d189
m-o-h-i-t <36105030+m-o-h-i-t@users.noreply.github.com>
2026-08-30 19:03:42 +0530

    Add: invert-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-0.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        def dfs(node):
+            if not node:
+                return None
+            left = dfs(node.left)
+            right = dfs(node.right)
+            node.left,node.right = right,left
+            return node
+        dfs(root)
+        return root
